6ed26d0be04ebe7e3efc8488f9da5d52f9ede340
vnedd <thangvu2098@gmail.com>
2022-08-22 19:24:47 +0700

    hi

--- assets/css/main.js
+++ assets/css/main.js
@@ -42,3 +42,29 @@ function hideRegisterModal() {
  for (const modalRegisterClose of modalRegisterCloses) {
     modalRegisterClose.addEventListener('click', hideRegisterModal )
  }
+
+
+
+//  product slide
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("product__info-image-item");
+  if (n > x.length) {
+    slideIndex = 1
+}
+  if (n < 1) {
+    slideIndex = x.length
+}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
